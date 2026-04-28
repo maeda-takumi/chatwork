a6b7d4bb69df36f22cb4d0347cbef33c99444700
maeda-takumi <jiangqiantian54@gmail.com>
2026-04-28 13:55:27 +0900

    14

--- header.php
+++ header.php
@@ -1,8 +1,18 @@
+<?php
+if (!headers_sent()) {
+  header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+  header('Pragma: no-cache');
+  header('Expires: 0');
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate, max-age=0">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Expires" content="0">
   <title>ChatSave</title>
   <link rel="stylesheet" href="style/style.css?v=<?php echo time(); ?>">
   <link rel="icon" href="img/icon.png?v=<?php echo time(); ?>" type="image/png">
